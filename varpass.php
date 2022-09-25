192fbbaece593d02b4f257a164373ef8f1f6e4d3
Dentonknifeworks <64991600+Dentonknifeworks@users.noreply.github.com>
2022-09-25 14:06:06 -0500

    Added return to Allsky and cleanup

---
+++ varpass.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+<form action="/index.php" Name="home">
+<input type="Submit" value="Return Home">
+<br><br>
+ 
+
+<?php
+$result = $_GET['newTime'];
+
+system('sudo timedatectl set-ntp false');
+echo 'Time is set to: ';
+$output = system("sudo date -s'$result' 2>&1");
+
+//echo "result is ";
+//echo $result;
+
+
+?>
+
+</body>
+</html>
